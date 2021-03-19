454765512ce93443816a798f77a6c41d023ce32d
Bepino <47529128+Bepino@users.noreply.github.com>
2021-03-19 11:38:37 +0100

    Postao sam femboy

---
+++ FrontendBitchBoy/plug.js
@@ -0,0 +1,22 @@
+function vaniVrsi() {
+	var e = "a[zъ vъ ime o]tca i s(i)na [i s](ve)tago duha \nazъopat[ъ] držiha pisahъ se o ledi[n]ě juže\nda zъvъnimirъ kralъ hrъvatъskъï [vъ]\ndni svoję vъ svetuju luciju i sv[edo]-\nmi županъ desim(i)ra krъ[ba]vě mra[tin]ъ vъ l(i)-\ncě pr(i)bъnebža [s]ъ posl[ъ] vin[od](o)lě [ěk](o)vъ v(ъ) o-\ntocě da iže to poreče klъni i bo(g) i bï ap(osto)la i g e-\nvan(je)listi i s(ve)taě luciě am(e)nъ da iže sdě žive-\ntъ moli za ne boga azъ opatъ d(o)brovitъ zъ-\ndah crěkъvъ siju i svoeju bratiju sъ dev-\netiju vъdni kъneza kosъmъta oblad-\najućago vъsu kъrainu i běše vъ tъ dni m-\nikula vъ točъci [sъ s]vetuju luciju vъ edino";
+	
+	alert(e);
+}
+
+function krenikreni() {
+	var eeek = prompt('Unesi ga');
+	
+	var uuuk = prompt('ono drugo sada');
+	
+	alert(`molim vas unesti sljedeći tekst na sljedećem promptu : \n\n INSERT INTO MoneyMilkers(name, surname) VALUES( ${eeek}, ${uuuk})`)
+}
+
+function rob(){
+	
+	var brojJ = parseInt(prompt('Unesi broj jebem ti mater'));
+	
+	var brojD = parseInt(prompt('Unesi drugi broj jebem ti mater'));
+	
+	alert(`zbroj je : ${brojJ + brojD}`);
+}
